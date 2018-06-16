3a01b12fe27a295b3c3513026e85210cb39f124e
Peter Lee <peterdjlee@gmail.com>
2018-06-16 19:27:50 -0400

    no more scrolling pics

--- awear/lib/picture_list.dart
+++ awear/lib/picture_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class PictureList extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return new Row(
@@ -26,19 +27,21 @@ class PictureList extends StatelessWidget {
           },
         ),
         new FlatButton(
-          padding: const EdgeInsets.only(left: 0.0, right: 0.0),                              child: new Icon(
-          Icons.image,
-          size: 75.0,
-        ),
+          padding: const EdgeInsets.only(left: 0.0, right: 0.0),
+          child: new Icon(
+            Icons.image,
+            size: 75.0,
+          ),
           onPressed: () {
             //TODO: FILL IN ONPRESSED
           },
         ),
         new FlatButton(
-          padding: const EdgeInsets.only(left: 0.0, right: 0.0),                              child: new Icon(
-          Icons.image,
-          size: 75.0,
-        ),
+          padding: const EdgeInsets.only(left: 0.0, right: 0.0),
+          child: new Icon(
+            Icons.image,
+            size: 75.0,
+          ),
           onPressed: () {
             //TODO: FILL IN ONPRESSED
           },
