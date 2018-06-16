c9ead68b99c7546bd2dabea916f437e608cc2324
Peter Lee <peterdjlee@gmail.com>
2018-06-16 12:58:05 -0400

    re-wrote layout; added bottom app bar

---
+++ awear/lib/awear_home.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class AwearHome extends StatelessWidget {
+
+  final topBar = new AppBar(
+    backgroundColor: new Color(0xfff8faf8),
+    centerTitle: true,
+    elevation: 1.0,
+    title: new Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        new Text(
+          'Awear',
+          style: new TextStyle(
+            color: Colors.black
+            //TODO: Add font
+          )
+        ),
+      ],
+    )
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: topBar,
+      body: new AwearBody(),
+      bottomNavigationBar: new Container(
+        color: Colors.white,
+        height: 50.0,
+        alignment: Alignment.center,
+        child: new BottomAppBar(
+          child: new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              new IconButton(
+                  icon: Icon(
+                    Icons.home,
+                  ),
+                  onPressed: () {},
+              ),
+              new IconButton(
+                icon: Icon(
+                  Icons.add_a_photo,
+                ),
+                onPressed: () {},
+              ),
+              new IconButton(
+                icon: Icon(
+                  Icons.account_circle,
+                ),
+                onPressed: () {},
+              ),
+            ],
+          )
+        )
+      ),
+    );
+  }
+}
--- awear/lib/main.dart
+++ awear/lib/main.dart
@@ -1,48 +1,18 @@
+import 'package:awear/awear_home.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Awear',
+    return MaterialApp(
+      title: "Awear",
       debugShowCheckedModeBanner: false,
       theme: new ThemeData(
-        primarySwatch: Colors.grey,
+        primaryColor: Colors.black,
       ),
-      home: new MyHomePage(title: "Awear"),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Row(mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              widget.title,
-              style: new TextStyle(
-                  color: Colors.white,
-              ),
-            )
-          ],
-        ),
-      ),
+      home: new AwearHome(),
     );
   }
 }
