eb1e1c09d06282b40d4571fb980f878bc618c5b6
Peter Lee <peterdjlee@gmail.com>
2018-06-16 18:25:34 -0400

    moved pic row into picture_list.dart

--- awear/lib/awear_list.dart
+++ awear/lib/awear_list.dart
@@ -1,3 +1,4 @@
+import 'package:awear/picture_list.dart';
 import 'package:flutter/material.dart';
 
 class AwearList extends StatelessWidget {
@@ -12,112 +13,70 @@ class AwearList extends StatelessWidget {
     movements.add(new Movement('Blazers','#GenderRoles','',''));
     movements.add(new Movement('No Bra','#FreeTheNipple','',''));
 
-    return new ListView.builder(
-        itemCount: movements.length,
-        itemBuilder: (context, index) {
-          return new Card(
-              child: new Container(
-                padding: new EdgeInsets.all(16.0),
-                child: new Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                        new FlatButton(
-                          padding: const EdgeInsets.only(left:16.0, right:16.0, top:0.0),
-                          child: new Text(
-                            movements[index].movementTitle,
-                            style: new TextStyle(
-                              fontFamily: 'PlayfairDisplay',
-                              fontSize: 30.0,
-                              fontStyle: FontStyle.italic,
+      return new ListView.builder(
+          itemCount: movements.length,
+          itemBuilder: (context, index) {
+            return new Card(
+                child: new Container(
+                  padding: new EdgeInsets.all(16.0),
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                          new FlatButton(
+                            padding: const EdgeInsets.only(left:16.0, right:16.0, top:0.0),
+                            child: new Text(
+                              movements[index].movementTitle,
+                              style: new TextStyle(
+                                fontFamily: 'PlayfairDisplay',
+                                fontSize: 30.0,
+                                fontStyle: FontStyle.italic,
+                              ),
                             ),
+                            onPressed: () {
+                              //TODO: FILL IN ONPRESSED
+                            },
                           ),
-                          onPressed: () {
-                            //TODO: FILL IN ONPRESSED
-                          },
-                        ),
-                        new Row(
-                          children: <Widget>[
-                            new FlatButton(
-                              padding: const EdgeInsets.only(left: 0.0, right: 0.0),
-                              child: new Icon(
-                                Icons.image,
-                                size: 75.0,
-                              ),
-                              onPressed: () {
-                                //TODO: FILL IN ONPRESSED
-                              },
-                            ),
-                            new FlatButton(
-                              padding: const EdgeInsets.only(left: 0.0, right: 0.0),
-                              child: new Icon(
-                                Icons.image,
-                                size: 75.0,
-                              ),
-                              onPressed: () {
-                                //TODO: FILL IN ONPRESSED
-                              },
-                            ),
-                            new FlatButton(
-                              padding: const EdgeInsets.only(left: 0.0, right: 0.0),                              child: new Icon(
-                                Icons.image,
-                                size: 75.0,
-                              ),
-                              onPressed: () {
-                                //TODO: FILL IN ONPRESSED
-                              },
-                            ),
-                            new FlatButton(
-                              padding: const EdgeInsets.only(left: 0.0, right: 0.0),                              child: new Icon(
-                              Icons.image,
-                              size: 75.0,
-                            ),
-                              onPressed: () {
-                                //TODO: FILL IN ONPRESSED
-                              },
+                          new PictureList(),
+                          new Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              new FlatButton(
+                                padding: const EdgeInsets.only(left: 16.0, right: 3.0),
+                                child: new Text(
+                                  movements[index].movementIssueOne,
+                                  style: new TextStyle(
+                                    fontFamily: 'PlayfairDisplay',
+                                    color: Colors.grey,
+                                    fontSize: 20.0,
+                                    fontStyle: FontStyle.italic,
+                                  ),
+                                ),
+                                  onPressed: () {
+                                  //TODO: FILL IN ONPRESSED
+                                  }),
+                              new FlatButton(
+                                child: new Text(
+                                  movements[index].movementIssueTwo,
+                                  style: new TextStyle(
+                                   fontFamily: 'PlayfairDisplay',
+                                    color: Colors.grey,
+                                  fontSize: 20.0,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                             ),
+                                onPressed: () {
+                                  //TODO: FILL IN ONPRESSED
+                                  },
                             ),
                           ],
-                        ),
-                    new Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        new FlatButton(
-                          padding: const EdgeInsets.only(left: 16.0, right: 3.0),
-                          child: new Text(
-                            movements[index].movementIssueOne,
-                            style: new TextStyle(
-                              fontFamily: 'PlayfairDisplay',
-                              color: Colors.grey,
-                              fontSize: 20.0,
-                              fontStyle: FontStyle.italic,
-                            ),
-                          ),
-                          onPressed: () {
-                            //TODO: FILL IN ONPRESSED
-                          },
-                        ),
-                        new FlatButton(
-                          child: new Text(
-                            movements[index].movementIssueTwo,
-                            style: new TextStyle(
-                              fontFamily: 'PlayfairDisplay',
-                              color: Colors.grey,
-                              fontSize: 20.0,
-                              fontStyle: FontStyle.italic,
-                            ),
-                          ),
-                          onPressed: () {
-                            //TODO: FILL IN ONPRESSED
-                          },
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              )
-          );
-        });
+                        )
+                     ],
+                   ),
+                )
+            );
+          });
+    }
   }
-}
 
 class Movement {
   final String movementTitle;
---
+++ awear/lib/picture_list.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class PictureList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Row(
+      children: <Widget>[
+        new FlatButton(
+          padding: const EdgeInsets.only(left: 0.0, right: 0.0),
+          child: new Icon(
+            Icons.image,
+            size: 75.0,
+          ),
+          onPressed: () {
+            //TODO: FILL IN ONPRESSED
+          },
+        ),
+        new FlatButton(
+          padding: const EdgeInsets.only(left: 0.0, right: 0.0),
+          child: new Icon(
+            Icons.image,
+            size: 75.0,
+          ),
+          onPressed: () {
+            //TODO: FILL IN ONPRESSED
+          },
+        ),
+        new FlatButton(
+          padding: const EdgeInsets.only(left: 0.0, right: 0.0),                              child: new Icon(
+          Icons.image,
+          size: 75.0,
+        ),
+          onPressed: () {
+            //TODO: FILL IN ONPRESSED
+          },
+        ),
+        new FlatButton(
+          padding: const EdgeInsets.only(left: 0.0, right: 0.0),                              child: new Icon(
+          Icons.image,
+          size: 75.0,
+        ),
+          onPressed: () {
+            //TODO: FILL IN ONPRESSED
+          },
+        ),
+      ],
+    );
+  }
+}
